93276b5bbb8d9b64573012888c95c3a9138ffa9d
Mohith <mohithjegan@gmail.com>
2024-11-14 22:01:47 -0500

    Add project files.

---
+++ School/Controllers/StudentPageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Models;
+
+namespace School.Controllers
+{
+    public class StudentPageController : Controller
+    {
+        // currently relying on the API to retrieve author information
+        // this is a simplified example. In practice, both the StudentAPI and StudentPage controllers
+        // should rely on a unified "Service", with an explicit interface
+        private readonly StudentAPIController _api;
+
+        public StudentPageController(StudentAPIController api)
+        {
+            _api = api;
+        }
+
+        // GET : StudentPage/List
+        public IActionResult List()
+        {
+            List<Student> Students = _api.ListStudents();
+            return View(Students);
+        }
+
+        // GET : StudentPage/Show
+        public IActionResult Show(int id) 
+        {
+            Student SelectedStudent = _api.FindStudent(id);
+            ViewData["Id"] = id;
+            return View(SelectedStudent);
+        }
+    }
+}
